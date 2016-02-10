520ebfef68feb4f6d55b94f3a5310799fa74ea1b
PhaniGaddipati <phanigaddipati@gmail.com>
2016-02-10 14:45:55 -0500

    blank app

--- SnaptronApp/SnaptronApp.js
+++ SnaptronApp/SnaptronApp.js
@@ -1,10 +1,8 @@
 Meteor.queriesDB = new Mongo.Collection("queries");
 
 if (Meteor.isClient) {
-    Template.test.events({
-        'click button': function () {
-            Meteor.call("getIdForQuery", "chr6:1-1000000|samples_count=5|");
-        }
+    Meteor.startup(function () {
+
     });
 }
 
