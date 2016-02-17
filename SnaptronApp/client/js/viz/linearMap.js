2506a5dd1621c42987ec4cab0522792e01729122
PhaniGaddipati <phanigaddipati@gmail.com>
2016-02-17 16:45:54 -0500

    viewbox for svg and using enter() with d3 so svg doesn't constantly have to be re-rendered

--- SnaptronApp/client/js/viz/linearMap.js
+++ SnaptronApp/client/js/viz/linearMap.js
@@ -2,7 +2,9 @@
  * Created by Phani on 2/14/2016.
  */
 
-const HEIGHT_MULTIPLIER = .4;
+const VIEWBOX_WIDTH = 1000;
+const VIEWBOX_HEIGHT = 400;
+
 const BACKGROUND_COLOR = "#FFFFFF";
 const MARKER_LABEL_STYLE = "fill:#EEEEEE;stroke:black;stroke-width:1";
 const MARKER_LINE_STYLE = "stroke:#DDDDDD;stroke-width:1";
@@ -12,53 +14,71 @@ const AXIS_K_CUTOFF = 10000;
 const MARKER_LABEL_HEIGHT = 25;
 
 var xScale;
+var start;
+var stop;
 
 Template.linearMap.onRendered(function () {
-    d3.select(window).on('resize', updateMap);
-    initMap();
+    //d3.select(window).on('resize', updateMap);
     updateMap();
 });
 
-function initMap() {
-    var svg = d3.select("#linearMapSVG");
-    initFrame(svg);
-}
-
 function updateMap() {
-    var svg = d3.select("#linearMapSVG");
-    // Set viewbox and have the svg fill the container (width)
-    svg.attr("width", "100%");
-    var width = svg.node().getBoundingClientRect().width;
-    var height = width * HEIGHT_MULTIPLIER;
-    svg.attr("viewBox 0 0 " + width + " " + height);
-    svg.attr("height", height);
+    var svg = d3.select(".svg-container").classed("svg-container", true)
+        .selectAll('svg').data([0])
+        .enter().append("svg")
+        .attr("class", "junctionmap")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 " + VIEWBOX_WIDTH + " " + VIEWBOX_HEIGHT)
+        .classed("svg-content-responsive", true)
+        .on("mouseout", removeMouseMarker)
+        .on("mousemove", updateMouseMarker);
 
     var junctions = Junctions.find().fetch();
-    var numJncts = junctions.length;
-
     var _limits = getLimits(junctions);
-    var start = _limits.start;
-    var stop = _limits.stop;
+    start = _limits.start;
+    stop = _limits.stop;
 
-    updateFrame(svg, width, height, start, stop);
+    updateFrame(svg);
+    updateJunctions(svg, junctions);
 }
 
-function initFrame(svg) {
-    svg.append("rect").attr("class", "background");
-    svg.append("g").attr("class", "xaxis")
-        .attr("transform", "translate(0,0)");
-    svg.on("mouseout", removeMouseMarker)
-        .on("mousemove", updateMouseMarker);
+function updateJunctions(svg, junctions) {
+    svg.selectAll(".jnct")
+        .data(junctions)
+        .enter()
+        .append("path")
+        .attr("d", junctionPath)
+        .attr("class", "jnct")
+        .attr("stroke", "black")
+        .attr("fill", "transparent");
 }
 
-function updateFrame(svg, width, height, start, stop) {
-    svg.select("background").attr("x", 0).attr("y", 0).attr("width", width)
-        .attr("height", height).attr("fill", BACKGROUND_COLOR);
+function junctionPath(jnct) {
+    var endpointY = VIEWBOX_HEIGHT - PADDING;
+    var startX = parseInt(xScale(jnct.start));
+    var endX = parseInt(xScale(jnct.end));
+    var range = endX - startX;
+    var cPoint1X = parseInt(startX + 2 * range / 6);
+    var cPoint2X = parseInt(startX + 4 * range / 6);
+    var cPointY = VIEWBOX_HEIGHT - PADDING - parseInt((VIEWBOX_HEIGHT - PADDING * 2) * (parseFloat(range) / (VIEWBOX_WIDTH / 3)));
+    cPointY = Math.max(PADDING, cPointY);
+    cPointY = Math.min(VIEWBOX_HEIGHT - PADDING - 5, cPointY);
+    return "M" + startX + " " + endpointY + " C " + cPoint1X + " " + cPointY + " " + cPoint2X + " " + cPointY + " " + endX + " " + endpointY;
+}
+
+function updateFrame(svg) {
+    svg.selectAll(".backgroundRect").data([0])
+        .enter()
+        .append("rect")
+        .attr("class", "backgroundRect")
+        .attr("transform", "translate(0,0)")
+        .attr("x", 0).attr("y", 0).attr("width", VIEWBOX_WIDTH)
+        .attr("height", VIEWBOX_HEIGHT).attr("fill", BACKGROUND_COLOR);
 
     //Draw axis
-    xScale = d3.scale.linear().range([PADDING, width - PADDING])
+    xScale = d3.scale.linear().range([PADDING, VIEWBOX_WIDTH - PADDING])
         .domain([start, stop]);
-    var numTicks = parseInt(width / 120);
+    var numTicks = parseInt(VIEWBOX_WIDTH / 120);
     var step = (stop - start) / (numTicks - 1);
     var tickValues = [start];
     for (var i = 0; i < numTicks - 1; i++) {
@@ -75,9 +95,12 @@ function updateFrame(svg, width, height, start, stop) {
             }
             return d;
         });
-    svg.transition()
-        .select("g.xaxis")
-        .attr("transform", "translate(0," + (height - PADDING) + ")")
+    svg.selectAll("g.xaxis").data([0])
+        .enter().append("g")
+        .attr("class", "xaxis")
+        .transition()
+        .attr("transform", "translate(0,0)")
+        .attr("transform", "translate(0," + (VIEWBOX_HEIGHT - PADDING) + ")")
         .call(xAxis);
 }
 
@@ -96,17 +119,16 @@ function getLimits(junctions) {
 }
 
 function addMouseMarker() {
-    var svg = d3.select("#linearMapSVG");
-    var marker = svg.select("g.mousemarker");
+    var marker = d3.select("g.mousemarker");
     if (marker.empty()) {
-        marker = svg.append("g").attr("class", "mousemarker");
+        marker = d3.select(".junctionmap").append("g").attr("class", "mousemarker");
         // Marker line
         marker.append("line")
             .attr("class", "markerline")
             .attr("x1", 0)
             .attr("y1", PADDING)
             .attr("x2", 0)
-            .attr("y2", svg.node().getBoundingClientRect().height - PADDING)
+            .attr("y2", VIEWBOX_HEIGHT - PADDING)
             .attr("style", MARKER_LINE_STYLE)
             .attr("pointer-events", "none");
         var label = marker.append("g").attr("class", "markerlabel").attr("transform", "translate(0,0)");
@@ -125,23 +147,20 @@ function addMouseMarker() {
             .attr("class", "markerlabeltext")
             .attr("pointer-events", "none")
             .attr("x", 10)
-            .attr("y", MARKER_LABEL_HEIGHT / 2 + 5)
-            .attr("text", "TEST");
+            .attr("y", MARKER_LABEL_HEIGHT / 2 + 5);
     }
 }
 
 function removeMouseMarker() {
-    var svg = d3.select("#linearMapSVG");
-    svg.selectAll("g.mousemarker").remove();
+    var svg = d3.selectAll("g.mousemarker").remove();
 }
 
 function updateMouseMarker() {
     addMouseMarker();
-    var svg = d3.select("#linearMapSVG");
     var coords = d3.mouse(this);
     coords[0] = Math.max(coords[0], PADDING);
-    coords[0] = Math.min(coords[0], svg.node().getBoundingClientRect().width - PADDING);
-    var marker = svg.select("g.mousemarker");
+    coords[0] = Math.min(coords[0], VIEWBOX_WIDTH - PADDING);
+    var marker = d3.select("g.mousemarker");
     marker.attr("transform", "translate(" + coords[0] + ",0)");
     var markerLabel = marker.select(".markerlabel");
     var label = markerLabel.select(".markerlabelbox");
@@ -155,7 +174,7 @@ function updateMouseMarker() {
     if (coords[0] - w - 50 <= 0) {
         //Goes offscreen, go to other side of line
         xOffset = 0;
-    } else if (coords[0] + w + 50 >= svg.node().getBBox().width) {
+    } else if (coords[0] + w + 50 >= VIEWBOX_WIDTH) {
         xOffset -= w / 2 + 10;
     }
     markerLabel.transition().attr("transform", "translate (" + xOffset + ",0)");
