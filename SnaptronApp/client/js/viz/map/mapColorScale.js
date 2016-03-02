90909cd735a7d8fd1de952df34ebc23c0a9293c5
PhaniGaddipati <phanigaddipati@gmail.com>
2016-03-02 08:30:33 -0500

    Scale labels. closes #26

--- SnaptronApp/client/js/viz/map/mapColorScale.js
+++ SnaptronApp/client/js/viz/map/mapColorScale.js
@@ -5,7 +5,8 @@
 
 updateColorScale = function (colorByKey, colorLog, junctions) {
     var colorScale;
-    if (colorByKey != null && colorByKey !== "bool") {
+    d3.select(".scale-container").selectAll("svg").remove();
+    if (colorByKey != null && JUNCTION_COLUMN_TYPES[colorByKey] !== "bool") {
 
         //TODO use mongo query to find max/min
 
@@ -35,11 +36,10 @@ updateColorScale = function (colorByKey, colorLog, junctions) {
 };
 
 function updateBar(colorScale) {
-    d3.select(".scale-container").selectAll("svg").remove();
-    var lowEnd = 1;
-    var highEnd = 100;
+    var lowEnd = 0;
+    var highEnd = SnaptronApp.LinearMap.COLOR_SCALE_VIEWBOX_W;
     var arr = [];
-    while (lowEnd <= highEnd) {
+    while (lowEnd < highEnd) {
         arr.push(lowEnd++);
     }
 
@@ -47,30 +47,55 @@ function updateBar(colorScale) {
         .data([0]).enter()
         .append("svg")
         .classed("svg-content-responsive", true)
-        .attr("viewBox", "0 0 " + 100 + " " + 15);
-    svg.selectAll("rect").data(arr)
-        .enter()
+        .attr("viewBox", "0 0 "
+            + SnaptronApp.LinearMap.COLOR_SCALE_VIEWBOX_W + " " + SnaptronApp.LinearMap.COLOR_SCALE_VIEWBOX_H);
+    var rects = svg.selectAll("rect").data(arr);
+    rects.enter()
         .append("rect")
         .attr("fill", function (d) {
-            var domain = colorScale.domain();
-            return colorScale(d * ((domain[1] - domain[0]) / 100) + domain[0]);
+            return colorScale(getValueFromScaleIndex(d, colorScale));
         })
         .attr("width", 1)
-        .attr("height", 2)
+        .attr("height", SnaptronApp.LinearMap.COLOR_SCALE_BAR_H)
         .attr("x", function (d) {
-            return d - 1;
+            return d;
         })
-        .attr("y", 5)
+        .attr("y", SnaptronApp.LinearMap.COLOR_SCALE_BAR_Y_OFF)
+        .attr("id", function (d) {
+            return "d" + d;
+        })
         .on("mouseover", function (d) {
-            svg.selectAll("text")
+            svg.selectAll("#scaleText")
                 .text(function () {
-                    var domain = colorScale.domain();
-                    return parseInt(d * ((domain[1] - domain[0]) / 100) + domain[0]);
+                    return parseInt(getValueFromScaleIndex(d, colorScale));
+                })
+                .attr("x", function () {
+                    return SnaptronApp.LinearMap.COLOR_SCALE_VIEWBOX_W / 2
+                        - this.getBBox().width / 2;
                 });
+            rects.attr("height", SnaptronApp.LinearMap.COLOR_SCALE_BAR_H);
+            svg.selectAll("#d" + d).data([d]).attr("height", SnaptronApp.LinearMap.COLOR_SCALE_SEL_BAR_H);
         });
     svg.append("text")
-        .attr("font-size", 3)
-        .attr("x", "1")
-        .attr("y", "11")
+        .attr("id", "scaleText")
+        .attr("font-size", SnaptronApp.LinearMap.COLOR_SCALE_TEXT_FONT_SIZE)
+        .attr("y", SnaptronApp.LinearMap.COLOR_SCALE_TEXT_Y_OFF)
         .text("");
+    svg.append("text")
+        .attr("font-size", SnaptronApp.LinearMap.COLOR_SCALE_TEXT_FONT_SIZE)
+        .attr("x", 0)
+        .attr("y", SnaptronApp.LinearMap.COLOR_SCALE_TEXT_Y_OFF)
+        .text(parseInt(getValueFromScaleIndex(lowEnd, colorScale)));
+    svg.append("text")
+        .attr("font-size", SnaptronApp.LinearMap.COLOR_SCALE_TEXT_FONT_SIZE)
+        .attr("y", SnaptronApp.LinearMap.COLOR_SCALE_TEXT_Y_OFF)
+        .text(parseInt(getValueFromScaleIndex(highEnd, colorScale)))
+        .attr("x", function () {
+            return SnaptronApp.LinearMap.COLOR_SCALE_VIEWBOX_W - this.getBBox().width;
+        });
+}
+
+function getValueFromScaleIndex(d, colorScale) {
+    var domain = colorScale.domain();
+    return d * ((domain[1] - domain[0]) / SnaptronApp.LinearMap.COLOR_SCALE_VIEWBOX_W) + domain[0];
 }
--- SnaptronApp/client/js/viz/map/namespace/visualDeclarations.js
+++ SnaptronApp/client/js/viz/map/namespace/visualDeclarations.js
@@ -4,15 +4,28 @@
 
 SnaptronApp.LinearMap = {};
 
+/**
+ * Frame constants.
+ * @type {number}
+ */
 SnaptronApp.LinearMap.VIEWBOX_WIDTH = 1000;
 SnaptronApp.LinearMap.VIEWBOX_HEIGHT = 350;
+SnaptronApp.LinearMap.MID_AXIS_LINE_HEIGHT = 4;
+SnaptronApp.LinearMap.AXIS_K_CUTOFF = 10000;
 
+/**
+ * Mouse marker constants.
+ * @type {string}
+ */
 SnaptronApp.LinearMap.MARKER_LABEL_STYLE = "fill:#EEEEEE;stroke:black;stroke-width:1";
 SnaptronApp.LinearMap.MARKER_LINE_STYLE = "stroke:#DDDDDD;stroke-width:1";
+SnaptronApp.LinearMap.MARKER_LABEL_HEIGHT = 25;
+
+/**
+ * Junction arc constants.
+ * @type {number}
+ */
 SnaptronApp.LinearMap.MIN_DISPLAY_LENGTH_PX = 3;
-
-SnaptronApp.LinearMap.MID_AXIS_LINE_HEIGHT = 4;
-
 SnaptronApp.LinearMap.JUNCTION_MAX_VAL_COLOR = "#d9230f";
 SnaptronApp.LinearMap.JUNCTION_NORMAL_COLOR = "black";
 SnaptronApp.LinearMap.JUNCTION_BOOL_TRUE_COLOR = "#d9230f";
@@ -22,5 +35,14 @@ SnaptronApp.LinearMap.JUNCTION_NORMAL_WIDTH = 2;
 SnaptronApp.LinearMap.JUNCTION_HIGHLIGHTED_WIDTH = 3;
 SnaptronApp.LinearMap.JUNCTION_SELECTED_WIDTH = 4;
 
-SnaptronApp.LinearMap.AXIS_K_CUTOFF = 10000;
-SnaptronApp.LinearMap.MARKER_LABEL_HEIGHT = 25;
+/**
+ * Color scale constants.
+ * @type {number}
+ */
+SnaptronApp.LinearMap.COLOR_SCALE_VIEWBOX_W = 100;
+SnaptronApp.LinearMap.COLOR_SCALE_VIEWBOX_H = 15;
+SnaptronApp.LinearMap.COLOR_SCALE_BAR_H = 3;
+SnaptronApp.LinearMap.COLOR_SCALE_SEL_BAR_H = 4;
+SnaptronApp.LinearMap.COLOR_SCALE_BAR_Y_OFF = 5;
+SnaptronApp.LinearMap.COLOR_SCALE_TEXT_FONT_SIZE = 3;
+SnaptronApp.LinearMap.COLOR_SCALE_TEXT_Y_OFF = 12;
