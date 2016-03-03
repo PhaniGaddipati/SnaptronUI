6cdc402ec70eb7285e2d0ca1ebfec95707d96b88
PhaniGaddipati <phanigaddipati@gmail.com>
2016-03-03 11:07:05 -0500

    fixed session.js..

--- SnaptronApp/lib/session.js
+++ SnaptronApp/lib/session.js
@@ -3,12 +3,14 @@
  */
 
 
-/**
- Session keys.
- */
-SnapApp.SESSION_SELECTED_JNCTS = "selectedJnctIDs";
+if (Meteor.isClient) {
+    /**
+     Session keys.
+     */
+    SnapApp.SESSION_SELECTED_JNCTS = "selectedJnctIDs";
 
-/**
- * Default session variable values.
- */
-Session.setDefault(SnapApp.SESSION_SELECTED_JNCTS, []);
+    /**
+     * Default session variable values.
+     */
+    Session.setDefault(SnapApp.SESSION_SELECTED_JNCTS, []);
+}
