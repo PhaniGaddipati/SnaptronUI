ed58b0f0b84a7c14d3bfdddb1cd00d1ae019f69d
PhaniGaddipati <phanigaddipati@gmail.com>
2016-03-10 08:52:04 -0500

    jk idk where those files came from

--- SnaptronApp/client/js/viz/table/namespace/mapDeclarations.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Phani on 3/2/2016.
- */
-SnapApp.Table = {};
-SnapApp.Table.DEFAULT_ENABLED_COLS = ["_id", "chromosome", "start", "end", "length", "samples_count", "coverage_sum", "coverage_avg", "coverage_med"];
