3d292dd635d50b1793a2f90fc1bdc34d955c1abb
PhaniGaddipati <phanigaddipati@gmail.com>
2016-03-11 09:27:30 -0500

    used underscore utils for min/max of colorbar

--- SnaptronApp/client/js/viz/map/mapColorScale.js
+++ SnaptronApp/client/js/viz/map/mapColorScale.js
@@ -7,19 +7,10 @@ updateColorScale = function (colorByKey, colorLog, junctions) {
     var colorScale;
     d3.select(".scale-container").selectAll("svg").remove();
     if (colorByKey != null && JNCT_COL_TYPES[colorByKey] !== "bool") {
+        var vals = _.pluck(junctions, colorByKey);
+        var colorByMin = _.min(vals);
+        var colorByMax = _.max(vals);
 
-        //TODO use mongo query to find max/min
-
-        var colorByMin = 9007199254740990;
-        var colorByMax = -9007199254740990;
-        for (var i = 0; i < junctions.length; i++) {
-            if (junctions[i][colorByKey] > colorByMax) {
-                colorByMax = junctions[i][colorByKey];
-            }
-            if (junctions[i][colorByKey] < colorByMin) {
-                colorByMin = junctions[i][colorByKey];
-            }
-        }
         if (colorLog) {
             colorScale = d3.scale.log();
         } else {
@@ -36,12 +27,7 @@ updateColorScale = function (colorByKey, colorLog, junctions) {
 };
 
 function updateBar(colorScale) {
-    var lowEnd = 0;
-    var highEnd = SnapApp.Map.SCALE_VIEWBOX_W;
-    var arr = [];
-    while (lowEnd < highEnd) {
-        arr.push(lowEnd++);
-    }
+    var indicies = _.range(0, SnapApp.Map.SCALE_VIEWBOX_W);
 
     var svg = d3.select(".scale-container").selectAll("svg")
         .data([0]).enter()
@@ -49,7 +35,7 @@ function updateBar(colorScale) {
         .classed("svg-content-responsive", true)
         .attr("viewBox", "0 0 "
             + SnapApp.Map.SCALE_VIEWBOX_W + " " + SnapApp.Map.SCALE_VIEWBOX_H);
-    var rects = svg.selectAll("rect").data(arr);
+    var rects = svg.selectAll("rect").data(indicies);
     rects.enter()
         .append("rect")
         .attr("fill", function (d) {
@@ -89,7 +75,7 @@ function updateBar(colorScale) {
     svg.append("text")
         .attr("font-size", SnapApp.Map.SCALE_TEXT_FONT_SIZE)
         .attr("y", SnapApp.Map.SCALE_TEXT_Y_OFF)
-        .text(parseInt(getValueFromScaleIndex(highEnd - 1, colorScale)))
+        .text(parseInt(getValueFromScaleIndex(SnapApp.Map.SCALE_VIEWBOX_W - 1, colorScale)))
         .attr("x", function () {
             return SnapApp.Map.SCALE_VIEWBOX_W - this.getBBox().width;
         });
