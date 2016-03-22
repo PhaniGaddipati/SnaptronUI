0672d737e6cc81d0c5e3b65cacfdc4f8d028fcbe
PhaniGaddipati <phanigaddipati@gmail.com>
2016-03-22 13:56:39 -0400

    formatted table better for samples

--- SnaptronApp/client/js/query/group/selectedSampleTable.js
+++ SnaptronApp/client/js/query/group/selectedSampleTable.js
@@ -2,13 +2,14 @@
  * Created by Phani on 3/22/2016.
  */
 
-var sampleLimit = 100;
+var SAMPLE_LIMIT  = 100;
+var INCLUDED_KEYS = ["run_accession_s", "sample_accession_s", "experiment_accession_s", "study_accession_s", "submission_accession_s", "sra_ID_s", "run_ID_s"];
 
 Template.selectedSampleTable.onCreated(function () {
     var self = this;
     self.autorun(function () {
         SnapApp.selectedJnctIDsDep.depend();
-        self.subscribe("samplesForJunctions", SnapApp.selectedJnctIDs, sampleLimit);
+        self.subscribe("samplesForJunctions", SnapApp.selectedJnctIDs, SAMPLE_LIMIT);
     });
 });
 
@@ -17,17 +18,26 @@ Template.selectedSampleTable.helpers({
         return SnapApp.SampleDB.findSamplesForJunctions(SnapApp.selectedJnctIDs);
     },
     "tableSettings": function () {
+        var samp   = Samples.findOne();
+        var fields = _.map(_.keys(samp), function (key) {
+            return {
+                key: key,
+                label: formatHeaderText(key),
+                hidden: (INCLUDED_KEYS.indexOf(key) == -1)
+            };
+        });
         return {
             "showColumnToggles": true,
             "showFilter": false,
             "rowsPerPage": 5,
             "responsive": true,
             "autoWidth": false,
-            "throttleRefresh": 1000
+            "throttleRefresh": 1000,
+            "fields": fields
         };
     }
 });
 
 function formatHeaderText(str) {
-    return str.toUpperCase().replace(/_/g, " ").trim();
+    return str.replace("_s", "").replace("_t", "").toUpperCase().replace(/_/g, " ").trim();
 }
